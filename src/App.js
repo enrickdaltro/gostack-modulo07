992834dda60558dc70d8c267dd79b70b65b65a98
Enrick <enrickdaltro@gmail.com>
2020-02-13 18:30:29 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
+import Routes from './routes';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+}
