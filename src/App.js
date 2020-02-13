329e73f79a2f0412d92ff028fccb840d221c6a2a
Enrick <enrickdaltro@gmail.com>
2020-02-13 18:43:33 -0300

    adding global style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
+import GlobalStyles from './styles/global';
 import Routes from './routes';
 
 // import { Container } from './styles';
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <GlobalStyles />
     </BrowserRouter>
   );
 }
