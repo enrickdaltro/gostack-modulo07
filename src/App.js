a13ba6c35de1c4626ed8dae461abed5f7c13c69a
Enrick <enrickdaltro@gmail.com>
2020-02-18 13:11:19 -0300

    adding redux to application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 import GlobalStyles from './styles/global';
 
@@ -7,14 +8,18 @@ import Header from './components/Header/index';
 
 import Routes from './routes';
 
+import store from './store/index';
+
 // import { Container } from './styles';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyles />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyles />
+      </BrowserRouter>
+    </Provider>
   );
 }
